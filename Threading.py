4171b8fe21ea7b22387f30e0cb12193a51e2a83f
JwillJwill <113475549+JwillJwill@users.noreply.github.com>
2022-09-22 19:34:07 -0400

    Add files via upload

---
+++ Threading.py
@@ -0,0 +1,17 @@
+import threading
+import time
+
+
+def func():
+    print('ran')
+    time.sleep(1)
+    print("done")
+    time.sleep(0.85)
+    print("now done")
+
+
+x = threading.Thread(target=func)
+x.start()
+print(threading.activeCount())
+time.sleep(0.9)
+print("finally")
